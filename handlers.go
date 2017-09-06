e5af5c301f199c2b1f8f42d34ef557505fee0270
Ben Paxton <ben.paxton@headforwards.com>
2017-09-06 11:24:01 +0100

    Add error to handler signature, add docstrings

--- handlers.go
+++ handlers.go
@@ -1,13 +1,16 @@
 package goflexer
 
-type Handler func(Event, *Context) Result
+// Handler represents a flexer handler
+type Handler func(Event, *Context) (Result, error)
 
 var handlers = map[string]Handler{}
 
+// GetHandlers returns a map of registered handlers
 func GetHandlers() map[string]Handler {
 	return handlers
 }
 
+// RegisterHandler registers a function against a given handler name
 func RegisterHandler(handler string, fn Handler) {
 	handlers[handler] = fn
 }
